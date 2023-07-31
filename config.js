c2ca32a1eebb527788e3a0ab30da1c222eb79bb1
Stupidsheepy <2983626014@qq.com>
2023-07-31 23:04:40 +0800

    fix: remove sth

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
-const apiKey = process.env.API_KEY;
-const url = process.env.URL;
 export default {
-    apiKey: apiKey,
-    url: url,
+    apiKey: "Bearer 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",
+    url: "https://service-2s9zw2zg-1318762081.usw.apigw.tencentcs.com/v1/chat/completions",
 };
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // import module so that I should use type="module" in html
-import config from './config.js';
+import config from "./config.mjs";
 
-let chat_area = document.querySelector('.chat-area');
-let messsage_area = document.querySelector('.message-area');
-let bot_avatar = document.querySelector('.bot-avatar-ball');
-let user_avatar = document.querySelector('.user-avatar-ball');
+let chat_area = document.querySelector(".chat-area");
+let messsage_area = document.querySelector(".message-area");
+let bot_avatar = document.querySelector(".bot-avatar-ball");
+let user_avatar = document.querySelector(".user-avatar-ball");
 let apiKey = config.apiKey;
 let url = config.url;
 // temperature for responses
@@ -14,71 +14,69 @@ let generateCounts = 1;
 
 // connect is the method, header and body of the request
 let connect = {
-    method: 'POST',
+    method: "POST",
     headers: {
         Authorization: apiKey,
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
     },
     body: JSON.stringify({
-        "model": "gpt-3.5-turbo",
-        "messages": [],
-        "temperature": temperature,
-        "n": generateCounts,
-    })
+        model: "gpt-3.5-turbo",
+        messages: [],
+        temperature: temperature,
+        n: generateCounts,
+    }),
 };
 
 // add new message to the body
 let addNewMessage = (message, role) => {
     // get the previous messages and add new message to it
-    let newMessage = { "role": role, "content": message };
-    let previousMessages = JSON.parse(connect['body'])['messages'];
+    let newMessage = { role: role, content: message };
+    let previousMessages = JSON.parse(connect["body"])["messages"];
     previousMessages.push(newMessage);
     let newBody = JSON.parse(connect.body);
-    newBody['messages'] = previousMessages;
+    newBody["messages"] = previousMessages;
     connect.body = JSON.stringify(newBody);
-}
+};
 
-// create send or receive time 
+// create send or receive time
 let createTimeBlock = () => {
     let time = new Date();
     let year = time.getFullYear();
     let month = time.getMonth() + 1;
-    if (month < 10) month = '0' + month.toString();
+    if (month < 10) month = "0" + month.toString();
     let date = time.getDate();
-    if (date < 10) date = '0' + date.toString();
+    if (date < 10) date = "0" + date.toString();
     let hour = time.getHours();
-    if (hour < 10) hour = '0' + hour.toString();
+    if (hour < 10) hour = "0" + hour.toString();
     let minute = time.getMinutes();
-    if (minute < 10) minute = '0' + minute.toString();
+    if (minute < 10) minute = "0" + minute.toString();
     let current_time = `${year}/${month}/${date}:${hour}:${minute}`;
     console.log(current_time);
 
-    let timeBlock = document.createElement('div');
-    timeBlock.classList.add('timeBlock');
+    let timeBlock = document.createElement("div");
+    timeBlock.classList.add("timeBlock");
     timeBlock.textContent = current_time;
     return timeBlock;
-}
-
+};
 
 // create message bubble
 let createMessageBubble = (message, role) => {
-    let roleBox = document.createElement('div');
-    roleBox.classList.add('role');
-    let messageBubble = document.createElement('div');
-    messageBubble.classList.add('message-bubble');
+    let roleBox = document.createElement("div");
+    roleBox.classList.add("role");
+    let messageBubble = document.createElement("div");
+    messageBubble.classList.add("message-bubble");
     messageBubble.classList.add(role);
     messageBubble.textContent = message;
     let timeBlock = createTimeBlock();
-    if (role === 'user') {
+    if (role === "user") {
         roleBox.appendChild(messageBubble);
-        let userIcon = document.createElement('div');
-        userIcon.classList.add('user-avatar-ball');
+        let userIcon = document.createElement("div");
+        userIcon.classList.add("user-avatar-ball");
         userIcon.appendChild(timeBlock);
         roleBox.appendChild(userIcon);
     } else {
-
-        let userIcon = document.createElement('div');
-        userIcon.classList.add('bot-avatar-ball');
+        let userIcon = document.createElement("div");
+        userIcon.classList.add("bot-avatar-ball");
         userIcon.appendChild(timeBlock);
         roleBox.appendChild(userIcon);
         roleBox.appendChild(messageBubble);
@@ -89,44 +87,42 @@ let createMessageBubble = (message, role) => {
     chat_area.removeAttribute("readonly");
     // auto scroll to the bottom
     roleBox.scrollIntoView(false);
-}
+};
 
 // for debounce function
 let chatAreaAfterSend = () => {
     chat_area.value = "Chat-bot is thinking now...";
     chat_area.style.color = "gray";
     chat_area.setAttribute("readonly", "");
-}
+};
 
-// send message to openai api and so on 
+// send message to openai api and so on
 let sendMessages = () => {
     chatAreaAfterSend();
     fetch(url, connect)
-        .then(res => res.json())
-        .then(text => {
+        .then((res) => res.json())
+        .then((text) => {
             // console.log(JSON.stringify(text, null, 4));
             // console.log(text['choices'][0]['message']);
-            let assistant = text['choices'][0]['message']['content'];
-            addNewMessage(assistant, 'assistant');
-            createMessageBubble(assistant, 'assistant')
-        })
-
-}
+            let assistant = text["choices"][0]["message"]["content"];
+            addNewMessage(assistant, "assistant");
+            createMessageBubble(assistant, "assistant");
+        });
+};
 
 // when user press enter, send message
 chat_area.onkeydown = (e) => {
     // console.log("what you press down is", e.code);
-    if (e.code !== 'Enter') return;
+    if (e.code !== "Enter") return;
     let userMessages = chat_area.value.trim();
-    if (userMessages === '') {
-        alert('Please input something');
+    if (userMessages === "") {
+        alert("Please input something");
         return;
     }
-    addNewMessage(userMessages, 'user')
+    addNewMessage(userMessages, "user");
     // console.log(userMessages);
-    createMessageBubble(userMessages, 'user');
+    createMessageBubble(userMessages, "user");
     sendMessages();
     // let chat area out of focus
     chat_area.blur();
-}
-
+};
